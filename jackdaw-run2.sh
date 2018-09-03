c2ed6173b0f0b53d8ef79c745f321e6db4042845
ZemTheMattress <ZemTheMattress@users.noreply.github.com>
2018-09-02 22:55:38 -0400

    comit

--- jackdaw-run2.sh
+++ jackdaw-run2.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
-DIRECTORY_TO_OBSERVE="/media/twittersync/"      // might want to change this
+DIRECTORY_TO_OBSERVE="/media/twittersync/"
 function block_for_change {
   inotifywait -r \
     -e modify,move,create,delete \
     $DIRECTORY_TO_OBSERVE
 }
-BUILD_SCRIPT="/media/jackdaw/jackdaw.sh"          // might want to change this too
+BUILD_SCRIPT="/media/jackdaw/jackdaw.sh"
 function build {
   bash $BUILD_SCRIPT
 }
